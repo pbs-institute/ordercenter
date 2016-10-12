53a839c39292f9856c38d6f0ac4a8966f0ed95c7
liurl3 <liurl3@asiainfo.com>
2016-10-12 16:27:54 +0800

    add mydatis mapper

---
+++ ordercenter-busi/src/main/java/com/drpeng/ordercenter/placeorder/controller/PlaceOrderController.java
@@ -0,0 +1,12 @@
+package com.drpeng.ordercenter.placeorder.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by liurl3 on 2016/10/12.
+ */
+@RestController
+@RequestMapping("/order")
+public class PlaceOrderController {
+}
